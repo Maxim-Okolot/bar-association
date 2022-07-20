9e967478496b6109100e562e318d7cdd56eebffe
Maxim Okolot <maximokolot@gmail.com>
2022-07-21 00:56:41 +0300

    add content

--- js/script.js
+++ js/script.js
@@ -261,15 +261,11 @@ jQuery(function ($) {
         searchControlProvider: 'yandex#search'
       }),
       objectManager = new ymaps.ObjectManager({
-        // Чтобы метки начали кластеризоваться, выставляем опцию.
         clusterize: true,
-        // ObjectManager принимает те же опции, что и кластеризатор.
         gridSize: 32,
         clusterDisableClickZoom: true
       });
 
-    // Чтобы задать опции одиночным объектам и кластерам,
-    // обратимся к дочерним коллекциям ObjectManager.
     objectManager.objects.options.set('preset', 'islands#greenDotIcon');
     objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
     myMap.geoObjects.add(objectManager);
